20b06e83e31f275dc4af48644942743f94545dfa
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-05-25 14:04:56 +0200

    Adding React-router to control comp display logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,71 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import styled from 'styled-components';
 import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 import './App.css';
-import Assets from './components/AssetList';
+import AssetList from './components/AssetList';
 import AssetForm from './components/AssetForm';
 
+
 const networkInterface = createNetworkInterface({
-  uri: '/graphiql'
+  uri: '/graphiql',
 });
 
 const client = new ApolloClient({
-  networkInterface: networkInterface
+  networkInterface,
 });
 
-const ContentWrapper = styled.section`
+const BaseWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   max-height: 100vh;
-  background: papayawhip;
-  overflow-y: scroll;
+`;
+
+const NavBar = styled.div`
+  height: 10%;
+  width: 90%;
+  position: fixed;
+  margin: 1em;
   display: flex;
   justify-content: space-between;
-`
+  background-color: papayawhip;
+`;
+
+const ContentWrapper = styled.div`
+  margin-top: 5em;
+  margin-right: 1em;
+  margin-left: 1em;
+`;
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <div className="App-header">
-          </div>
-          <ContentWrapper>
-            <Assets />
-            <AssetForm />
-          </ContentWrapper>
-        </div>
+        <Router>
+          <BaseWrapper>
+            <NavBar>
+              <Link to="/asset-list">
+                List
+              </Link>
+              <Link to="/asset-form">
+                Form
+              </Link>
+            </NavBar>
+            <ContentWrapper>
+              <Switch>
+                <Route path="/asset-form" component={AssetForm} />
+                <Route path="/asset-list" component={AssetList} />
+                <Redirect to="/asset-form" />
+              </Switch>
+            </ContentWrapper>
+          </BaseWrapper>
+        </Router>
       </ApolloProvider>
     );
   }
