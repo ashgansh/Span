c4235ac51eb56421fc874f79954ce2fe37d20b5d
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-12-03 23:34:04 +0100

    Rename web to backend

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 docker-compose up -d postgres
+docker-compose up -d backend
 
-docker-compose run web mix deps.get 
-docker-compose run web mix ecto.create
-docker-compose run web mix ecto.migrate
-docker-compose run web mix run priv/repo/seeds.exs
+docker-compose run backend mix deps.get 
+docker-compose run backend mix ecto.create
+docker-compose run backend mix ecto.migrate
+docker-compose run backend mix run priv/repo/seeds.exs
